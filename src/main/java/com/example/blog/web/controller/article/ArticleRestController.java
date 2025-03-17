cc131be80c0d7e3fd36414b0f2051d9ec44d5714
tokotoko666 <cappuccino969@gmail.com>
2025-03-17 21:47:28 +0900

    Controller and DTO

---
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -0,0 +1,22 @@
+package com.example.blog.web.controller.article;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class ArticleRestController {
+
+    @GetMapping("/articles/{id}")
+    public ArticleDTO showArticle(@PathVariable long id) {
+        return new ArticleDTO(
+                id,
+                "This is title: id = " + id,
+                "This is content",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+}
