277a2b4695714c41b391c78e33ab6f1c70d02082
tokotoko666 <cappuccino969@gmail.com>
2025-03-22 18:17:58 +0900

    ArticleRepository作成

---
+++ src/main/java/com/example/blog/repository/article/ArticleRepository.java
@@ -0,0 +1,24 @@
+package com.example.blog.repository.article;
+
+import com.example.blog.web.controller.article.service.article.ArticleEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Optional;
+
+@Mapper
+public interface ArticleRepository {
+
+    @Select("""
+            SELECT
+                id
+              , title
+              , body
+              , created_at
+              , updated_at
+            FROM articles
+            WHERE id = #{id}
+            """)
+    Optional<ArticleEntity> selectById(@Param("id") long id);
+}
--- src/main/java/com/example/blog/web/controller/article/service/article/ArticleService.java
+++ src/main/java/com/example/blog/web/controller/article/service/article/ArticleService.java
@@ -1,22 +1,18 @@
 package com.example.blog.web.controller.article.service.article;
 
+import com.example.blog.repository.article.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
 
+    private final ArticleRepository articleRepository;
+
     public Optional<ArticleEntity> findById(long id) {
-        return Optional.of(
-                new ArticleEntity(
-                        id,
-                        "title",
-                        "content",
-                        LocalDateTime.now(),
-                        LocalDateTime.now()
-                ));
+        return articleRepository.selectById(id);
     }
 }
