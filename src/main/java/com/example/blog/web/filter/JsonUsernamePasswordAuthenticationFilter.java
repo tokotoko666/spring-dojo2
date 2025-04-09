22217bcce8d1b8778552e94f1457eb23155d37f1
tokotoko666 <cappuccino969@gmail.com>
2025-04-10 08:44:16 +0900

    自作Filterから常に認証成功のダミーユーザーを返す

--- src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -2,14 +2,20 @@ package com.example.blog.web.filter;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 public class JsonUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        return UsernamePasswordAuthenticationToken.authenticated(
+                "dummy-user"
+                , "dummy-password"
+                , List.of());
     }
 }
