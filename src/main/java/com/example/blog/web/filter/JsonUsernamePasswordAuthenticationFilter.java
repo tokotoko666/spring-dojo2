b39413eeda65f9013aabbc2d554fec89c279efe0
tokotoko666 <cappuccino969@gmail.com>
2025-04-20 09:59:44 +0900

    attemptAuthenticationメソッドをRepositoryから取得する

--- src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -1,7 +1,10 @@
 package com.example.blog.web.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -9,16 +12,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.context.SecurityContextRepository;
 
-import java.util.List;
+import java.io.IOException;
 
 public class JsonUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private final ObjectMapper objectMapper;
+
     public JsonUsernamePasswordAuthenticationFilter(
             SecurityContextRepository securityContextRepository,
-            SessionAuthenticationStrategy sessionAuthenticationStrategy) {
+            SessionAuthenticationStrategy sessionAuthenticationStrategy,
+            AuthenticationManager authenticationManager,
+            ObjectMapper objectMapper) {
         super();
+        this.objectMapper = objectMapper;
         setSecurityContextRepository(securityContextRepository);
         setSessionAuthenticationStrategy(sessionAuthenticationStrategy);
+        setAuthenticationManager(authenticationManager);
         setAuthenticationSuccessHandler((req, res, auth) -> {
             res.setStatus(HttpServletResponse.SC_OK);
         });
@@ -30,9 +39,23 @@ public class JsonUsernamePasswordAuthenticationFilter extends UsernamePasswordAu
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
-        return UsernamePasswordAuthenticationToken.authenticated(
-                "dummy-user",
-                "dummy-password",
-                List.of());
+        LoginRequest jsonRequest = null;
+        try {
+            jsonRequest = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
+        } catch (IOException e) {
+            throw new AuthenticationServiceException("fail to read request as json", e);
+        }
+
+        var username = jsonRequest.username != null ? jsonRequest.username : "";
+        var password = jsonRequest.password != null ? jsonRequest.password : "";
+
+        var authRequest = UsernamePasswordAuthenticationToken.unauthenticated(username, password);
+        // Allow subclasses to set the "details" property
+        setDetails(request, authRequest);
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    private record LoginRequest(String username, String password) {
+
     }
 }
