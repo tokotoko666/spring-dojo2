d7d684554ac3e59d26947df74f368ad4c02877ec
tokotoko666 <cappuccino969@gmail.com>
2025-04-20 09:57:56 +0900

    CustomUserDetailsServiceを作成

---
+++ src/main/java/com/example/blog/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.blog.security;
+
+import com.example.blog.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.selectByUsername(username)
+                .map(r -> User.builder()
+                        .username(r.username())
+                        .password(r.password())
+                        .disabled(!r.enabled())
+                        .build()
+                )
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "given username is not found: username = " + username
+                ));
+    }
+}
