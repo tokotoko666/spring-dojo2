65e5039b960c84b0f2cc22e8348e41ebf9096f7a
tokotoko666 <cappuccino969@gmail.com>
2025-04-30 22:43:42 +0900

    登録処理を実装

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -51,11 +52,11 @@ public class SecurityConfig {
                         UsernamePasswordAuthenticationFilter.class)
                 .securityContext(context -> context.securityContextRepository(securityContextRepository))
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers("/articles/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .exceptionHandling(customizer -> customizer.accessDeniedHandler((req, res, ex) -> {
+                .exceptionHandling(customizer -> customizer.accessDeniedHandler((req, res, auth) -> {
                     res.setStatus(HttpServletResponse.SC_FORBIDDEN);
                 }));
 
