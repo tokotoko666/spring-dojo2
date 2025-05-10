ccd05a4b9165e35f2dd2db19b03c52dcdc8c12ec
tokotoko666 <cappuccino969@gmail.com>
2025-05-10 21:36:51 +0900

    Topページへアクセス

--- src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
+++ src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -13,8 +15,42 @@ public class RegistrationAndLoginIT {
     @Autowired
     private WebTestClient webTestClient;
 
+
     @Test
     public void integrationTest() {
-        assertThat(webTestClient).isNotNull();
+
+        var xsrfToken = getRoot();
+
+        // ユーザー登録
+
+        //ログイン失敗
+        //Cookie に XSRF-TOKEN がない
+        //ヘッダーに X-XSRF-TOKEN がない
+        //Cookie の XSRF-TOKEN とヘッダーの X-XSRF-TOKEN の値が異なる
+        //ユーザー名が存在しない
+        //パスワードがデータベースに保存されているパスワードと違う
+        //ログイン成功
+        //ユーザー名がデータベースに存在する
+        //パスワードがデータベースに保存されているパスワードと違う
+        //Cookie の XSRF-TOKEN とヘッダーの X-XSRF-TOKEN の値が一致する
+        //→ 200 OK が返る
+        //→ レスポンスに Set-Cookie: JSESSIONID が返ってくる
+    }
+
+    private String getRoot() {
+        // ## Arrange ##
+
+        // ## Act ##
+        var responseSpec = webTestClient.get().uri("/").exchange();
+
+        // ## Assert ##
+        var response = responseSpec.returnResult(String.class);
+        var xsrfTokenOpt = Optional.ofNullable(response.getResponseCookies().getFirst("XSRF-TOKEN"));
+
+        responseSpec.expectStatus().isNoContent();
+        assertThat(xsrfTokenOpt).isPresent()
+                .hasValueSatisfying(xsrfTokenCookie -> assertThat(xsrfTokenCookie.getValue()).isNotBlank());
+
+        return xsrfTokenOpt.get().getValue();
     }
 }
