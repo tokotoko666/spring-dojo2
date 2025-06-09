fc667c19c8c48640874ade32b4d5bea6dcfc30f8
tokotoko666 <cappuccino969@gmail.com>
2025-06-10 08:24:14 +0900

    初期passwordのエンコード化

--- src/main/resources/db/migration_dev/R__Insert_sample_data.sql
+++ src/main/resources/db/migration_dev/R__Insert_sample_data.sql
@@ -10,8 +10,9 @@ INSERT INTO articles (title, body)
 
 DELETE FROM users;
 
+-- password is "password" for all users
 INSERT INTO users(username, password, enabled)
- VALUES('user1', 'password', true)
-     , ('user2', 'password', true)
-     , ('user3', 'password', true)
+ VALUES('user1', '$2a$10$F5JP83Nje/nst4yLgRTRVeseBIYitiDS71TDNIK5CZ6wEm6byWYTa', true)
+     , ('user2', '$2a$10$JctG3h1.BTvVLvU26r0mzeYENPNYcMts/y5U.1I028hwmqZsO18si', true)
+     , ('user3', '$2a$10$R5roJSqLKSEPGXFsYqoAUuBq/8xe0KpVUzbMWWjN4Th54/K9I8lfC', true)
 ;
--- src/test/java/com/example/blog/SomeTrialTest.java
+++ src/test/java/com/example/blog/SomeTrialTest.java
@@ -1,18 +1,27 @@
 package com.example.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Base64;
 
-public class SpringSessionBase64Test {
+public class SomeTrialTest {
 
     @Test
-    public void test() {
+    public void base64() {
         var uuid = "2471b8da-95d0-47f1-bbb6-fc3601d5ac86";
         var base64Bytes = Base64.getEncoder().encode(uuid.getBytes());
         var base64String = new String(base64Bytes);
         System.out.println(base64String);
         // MjQ3MWI4ZGEtOTVkMC00N2YxLWJiYjYtZmMzNjAxZDVhYzg2
         // MjQ3MWI4ZGEtOTVkMC00N2YxLWJiYjYtZmMzNjAxZDVhYzg2
+    }
+
+    @Test
+    public void bcrypt() {
+        var encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("password"));
+        System.out.println(encoder.encode("password"));
+        System.out.println(encoder.encode("password"));
     }
 }
