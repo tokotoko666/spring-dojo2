693e435bbfe576d6598033ed0537bac863a796fc
tokotoko666 <cappuccino969@gmail.com>
2025-06-13 22:39:41 +0900

    Mockを使ったServiceのテスト

---
+++ src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceMockTest.java
@@ -0,0 +1,39 @@
+package com.example.blog.web.controller.article.service.article;
+
+import com.example.blog.repository.article.ArticleRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceMockTest {
+
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @InjectMocks
+    private ArticleService cut;
+
+    @Test
+    public void cut() {
+        assertThat(cut).isNotNull();
+    }
+
+    @Test
+    public void mockPractice() {
+        when(articleRepository.selectById(999)).thenReturn(Optional.of(new ArticleEntity(999, null, null, null, null)));
+
+        assertThat(articleRepository.selectById(999)).isPresent().hasValueSatisfying(article -> {
+            assertThat(article.id()).isEqualTo(999);
+        });
+        assertThat(articleRepository.selectById(111)).isEmpty();
+    }
+
+}
