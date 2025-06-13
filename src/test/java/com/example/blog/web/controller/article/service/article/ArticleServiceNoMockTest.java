9af679d37f90b35b7eaa5709bf5a81052c9406a8
tokotoko666 <cappuccino969@gmail.com>
2025-06-13 22:38:57 +0900

    Mockを使わないServiceのテスト

---
+++ src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceNoMockTest.java
@@ -0,0 +1,61 @@
+package com.example.blog.web.controller.article.service.article;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class ArticleServiceNoMockTest {
+
+    @Autowired
+    private ArticleService cut;
+
+    @Test
+    public void cut() {
+        assertThat(cut).isNotNull();
+    }
+
+    @Test
+    @DisplayName("findById: 指定されたIDのArticleが存在するとき、ArticleEntityを返す")
+    @Sql(statements = {
+            """
+            INSERT INTO articles (id, title, body, created_at, updated_at)
+            VALUES (999, 'title_999', 'body_999', '2022-01-01 00:00:00', '2022-02-01 00:00:00');
+            """
+
+    })
+    public void findBy_returnArticleEntity() {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = cut.findById(999);
+
+        // ## Assert ##
+        assertThat(actual).isPresent().hasValueSatisfying(article -> {
+            assertThat(article.id()).isEqualTo(999);
+            assertThat(article.title()).isEqualTo("title_999");
+            assertThat(article.content()).isEqualTo("body_999");
+            assertThat(article.createdAt()).isEqualTo("2022-01-01T00:00:00");
+            assertThat(article.updatedAt()).isEqualTo("2022-02-01T00:00:00");
+
+        });
+    }
+
+    @Test
+    @DisplayName("findById: 指定されたIDのArticleが存在しないとき、Optional.Emptyを返す")
+    public void findBy_returnEmpty() {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = cut.findById(999);
+
+        // ## Assert ##
+        assertThat(actual).isEmpty();
+    }
+}
