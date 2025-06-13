413412b6715c668ad2a8087be07b65f642baa60f
tokotoko666 <cappuccino969@gmail.com>
2025-06-13 22:57:41 +0900

    Mockを使ったServiceのfindByテスト

--- src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceMockTest.java
+++ src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceMockTest.java
@@ -1,12 +1,14 @@
 package com.example.blog.web.controller.article.service.article;
 
 import com.example.blog.repository.article.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,6 +36,45 @@ class ArticleServiceMockTest {
             assertThat(article.id()).isEqualTo(999);
         });
         assertThat(articleRepository.selectById(111)).isEmpty();
+    }
+
+    @Test
+    @DisplayName("findById: 指定されたIDのArticleが存在するとき、ArticleEntityを返す")
+    public void findBy_returnArticleEntity() {
+        // ## Arrange ##
+        when(articleRepository.selectById(999)).thenReturn(Optional.of(
+                new ArticleEntity(
+                        999,
+                        "title_999",
+                        "body_999",
+                        LocalDateTime.of(2022,1,1,10,0,0,0),
+                        LocalDateTime.of(2022,2,1,11,0,0,0))
+        ));
+
+        // ## Act ##
+        var actual = cut.findById(999);
+
+        // ## Assert ##
+        assertThat(actual).isPresent().hasValueSatisfying(article -> {
+            assertThat(article.id()).isEqualTo(999);
+            assertThat(article.title()).isEqualTo("title_999");
+            assertThat(article.content()).isEqualTo("body_999");
+            assertThat(article.createdAt()).isEqualTo("2022-01-01T10:00:00");
+            assertThat(article.updatedAt()).isEqualTo("2022-02-01T11:00:00");
+        });
+    }
+
+    @Test
+    @DisplayName("findById: 指定されたIDのArticleが存在しないとき、Optional.Emptyを返す")
+    public void findBy_returnEmpty() {
+        // ## Arrange ##
+        when(articleRepository.selectById(999)).thenReturn(Optional.empty());
+
+        // ## Act ##
+        var actual = cut.findById(999);
+
+        // ## Assert ##
+        assertThat(actual).isEmpty();
     }
 
 }
