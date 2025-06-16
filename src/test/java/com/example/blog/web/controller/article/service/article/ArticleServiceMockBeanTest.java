fbbb09393bb0a083340be3d74dafe3432eef4e4e
tokotoko666 <cappuccino969@gmail.com>
2025-06-16 22:27:04 +0900

    Mockを使ったServiceのテストを作成

--- src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceMockBeanTest.java
+++ src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceMockBeanTest.java
@@ -1,11 +1,12 @@
 package com.example.blog.web.controller.article.service.article;
 
+import com.example.blog.config.MybatisDefaultDatasourceTest;
 import com.example.blog.repository.article.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -13,7 +14,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@MybatisDefaultDatasourceTest
+@Import(ArticleService.class)
 class ArticleServiceMockBeanTest {
 
     @MockBean
