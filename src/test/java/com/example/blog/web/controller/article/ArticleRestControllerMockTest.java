a3f9654be58303eeaa8147e6910280324d57ec9d
tokotoko666 <cappuccino969@gmail.com>
2025-06-21 18:46:28 +0900

    Mockを使ったControllerのテストを作成

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerMockTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerMockTest.java
@@ -1,15 +1,29 @@
 package com.example.blog.web.controller.article;
 
+import com.example.blog.config.ObjectMapperConfig;
+import com.example.blog.config.SecurityConfig;
+import com.example.blog.web.controller.article.service.article.ArticleEntity;
 import com.example.blog.web.controller.article.service.article.ArticleService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ArticleRestController.class)
+@Import({ObjectMapperConfig.class, SecurityConfig.class})
 class ArticleRestControllerMockTest {
 
     @Autowired
@@ -18,8 +32,46 @@ class ArticleRestControllerMockTest {
     @MockBean
     private ArticleService mockArticleService;
 
+    @MockBean
+    private UserDetailsService mockUserDetailsService;
+
     @Test
     public void mockMvc() {
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    @DisplayName("GET /articles/{id}：指定されたIDの記事が存在するとき、200OK")
+    public void getArticlesById_200OK() throws Exception {
+        // ## Arrange ##
+        var expected = new ArticleEntity(999,
+                "title_999",
+                "content_999",
+                LocalDateTime.of(2022, 1, 2, 3, 4, 5),
+                LocalDateTime.of(2023, 1, 2, 3, 4, 5));
+
+        when(mockArticleService.findById(expected.id())).thenReturn(Optional.of(expected));
+        // ## Act ##
+        var actual = mockMvc.perform(get("/articles/{id}", expected.id()));
+
+        // ## Assert ##
+        actual.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(expected.id()))
+                .andExpect(jsonPath("$.title").value(expected.title()))
+                .andExpect(jsonPath("$.content").value(expected.content()))
+                .andExpect(jsonPath("$.createdAt").value(expected.createdAt().toString()))
+                .andExpect(jsonPath("$.updatedAt").value(expected.updatedAt().toString()));
+    }
+
+    public void getArticlesById_404NotFound() throws Exception {
+        // ## Arrange ##
+        var expectedId = 999;
+
+        when(mockArticleService.findById(expectedId)).thenReturn(Optional.empty());
+        // ## Act ##
+        var actual = mockMvc.perform(get("/articles/{id}", expectedId));
+
+        // ## Assert ##
+        actual.andExpect(status().isNotFound());
     }
 }
