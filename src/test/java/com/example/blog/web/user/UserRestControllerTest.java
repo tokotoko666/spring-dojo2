2fbf26ab5400dc3cde75163ac33d3cecf40d29ab
tokotoko666 <cappuccino969@gmail.com>
2025-06-21 18:48:06 +0900

    ログイン済のControllerのテストを作成（@WithMockUser利用）

---
+++ src/test/java/com/example/blog/web/user/UserRestControllerTest.java
@@ -0,0 +1,54 @@
+package com.example.blog.web.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserRestControllerTest {
+
+    private final String MOCK_USERNAME = "user1";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void mockMvc() {
+        assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    @DisplayName("/users/me: ログインユーザーがアクセスすると、200 OK でユーザー名を返す")
+    @WithMockUser(username = MOCK_USERNAME)
+    public void usersMe_return200() throws Exception {
+        // ## Arrange ##
+        // ## Assert ##
+        var acutual = mockMvc.perform(MockMvcRequestBuilders.get("/users/me"));
+
+        // ## Assert ##
+        acutual.andExpect(status().isOk())
+                .andExpect(content().bytes(MOCK_USERNAME.getBytes()));
+    }
+
+    @Test
+    @DisplayName("/users/me: 未ログインユーザーがアクセスすると、403 Forbiden を返す")
+    public void usersMe_return403() throws Exception {
+        // ## Arrange ##
+
+        // ## Assert ##
+        var actual = mockMvc.perform(MockMvcRequestBuilders.get("/users/me"));
+
+        // ## Assert ##
+        actual.andExpect(status().isForbidden());
+    }
+}
