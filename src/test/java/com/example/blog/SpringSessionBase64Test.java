dda5dc8e03bb9e610e8005da7afdec07ddf3fd83
tokotoko666 <cappuccino969@gmail.com>
2025-06-30 10:11:39 +0900

    Base64の確認

---
+++ src/test/java/com/example/blog/SpringSessionBase64Test.java
@@ -0,0 +1,17 @@
+package com.example.blog;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Base64;
+
+public class SpringSessionBase64Test {
+
+    @Test
+    public void test() {
+        var uuid = "a70368f7-0237-4684-9c96-ae7b81b0920a";
+        var encoded64Bytes = Base64.getEncoder().encode(uuid.getBytes());
+        var encoded64String = new String(encoded64Bytes);
+        System.out.println(encoded64String);
+        // YTcwMzY4ZjctMDIzNy00Njg0LTljOTYtYWU3YjgxYjA5MjBh
+    }
+}
