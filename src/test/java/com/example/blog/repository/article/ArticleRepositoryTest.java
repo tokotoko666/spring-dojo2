e146ab91b1f3ac83f2c273612e46ced88b553904
tokotoko666 <cappuccino969@gmail.com>
2025-07-01 08:31:09 +0900

    Repositoryのテストクラスを作成

---
+++ src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.example.blog.repository.article;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ArticleRepositoryTest {
+
+    @Autowired
+    private ArticleRepository cut;
+
+    @Test
+    public void test() {
+        assertThat(cut).isNotNull();
+    }
+
+    @Test
+    @DisplayName("selectById: 指定されたIDの記事が存在するとき、ArticleEntityを返す")
+    @Sql(statements = {"""
+            INSERT INTO articles (id, title, body, created_at, updated_at)
+            VALUES (999, 'title_999', 'body_999', '2010-10-01 00:00:00', '2010-11-01 00:00:00');
+            """
+    })
+    public void selectById_returnArticleEntity() {
+        // ## Arrange ##
+        // ## Act ##
+        var actual = cut.selectById(999);
+
+        // ## Assert ##
+        assertThat(actual)
+                .isPresent()
+                .hasValueSatisfying(article -> {
+                    assertThat(article.id()).isEqualTo(999);
+                    assertThat(article.title()).isEqualTo("title_999");
+                    assertThat(article.content()).isEqualTo("body_999");
+                    assertThat(article.createdAt()).isEqualTo("2010-10-01T00:00:00");
+                    assertThat(article.updatedAt()).isEqualTo("2010-11-01T00:00:00");
+                });
+    }
+
+    @Test
+    @DisplayName("selectById: 指定されたIDの記事が存在しないとき、Optional.emptyを返す")
+    public void selectById_returnEmpty() {
+        // ## Arrange ##
+        // ## Act ##
+        var actual = cut.selectById(999);
+
+        // ## Assert ##
+        assertThat(actual).isEmpty();}
+}
