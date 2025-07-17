e4305466e00583a058984626914ff433296826e7
tokotoko666 <cappuccino969@gmail.com>
2025-07-17 22:26:20 +0900

    ExceptionHandlerAdviseクラスを作成

---
+++ src/test/java/com/example/blog/web/advise/ExceptionHandlerAdvise.java
@@ -0,0 +1,25 @@
+package com.example.blog.web.advise;
+
+import com.example.blog.model.InternalServerError;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvise {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<InternalServerError> handleInternalServerError(RuntimeException e) {
+        var error = new InternalServerError();
+        error.setType(null);
+        error.setTitle("Internal Server Error");
+        error.setStatus(500);
+        error.setDetail(null);
+        error.setInstance(null);
+
+        return ResponseEntity.internalServerError()
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(error);
+    }
+}
