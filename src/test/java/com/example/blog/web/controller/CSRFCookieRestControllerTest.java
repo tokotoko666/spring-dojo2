312d07f0aa637beda4acfb2dc04e01649dba3db2
tokotoko666 <cappuccino969@gmail.com>
2025-08-02 14:28:21 +0900

    @Nestedを使ってリファクタリング

--- src/test/java/com/example/blog/web/controller/CSRFCookieRestControllerTest.java
+++ src/test/java/com/example/blog/web/controller/CSRFCookieRestControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.blog.web.controller;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,19 +19,22 @@ import static org.hamcrest.core.StringContains.containsString;
 @AutoConfigureMockMvc
 class CSRFCookieRestControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Nested
+    class Return204 {
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("GET /csrf-cookie:204　を返し　Set-Cookie　ヘッダに　XSRF-TOKEN　が含まれている")
-    void return204() throws Exception {
-        // ## Arrange ##
+        @Test
+        @DisplayName("GET /csrf-cookie:204　を返し　Set-Cookie　ヘッダに　XSRF-TOKEN　が含まれている")
+        void return204() throws Exception {
+            // ## Arrange ##
 
-        // ## Act ##
-        var result = mockMvc.perform(get("/csrf-cookie"));
+            // ## Act ##
+            var result = mockMvc.perform(get("/csrf-cookie"));
 
-        // ## Assert ##
-        result.andExpect(status().isNoContent())
-                .andExpect(header().string("Set-Cookie", containsString("XSRF-TOKEN")));
+            // ## Assert ##
+            result.andExpect(status().isNoContent())
+                    .andExpect(header().string("Set-Cookie", containsString("XSRF-TOKEN")));
+        }
     }
 }
