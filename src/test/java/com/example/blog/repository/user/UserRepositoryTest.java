8de4db9cd046c48de4089368fb22afee3c581d38
tokotoko666 <cappuccino969@gmail.com>
2025-08-11 21:19:22 +0900

    selectByUsernameのテストを書く

--- src/test/java/com/example/blog/repository/user/UserRepositoryTest.java
+++ src/test/java/com/example/blog/repository/user/UserRepositoryTest.java
@@ -1,11 +1,12 @@
 package com.example.blog.repository.user;
 
 import com.example.blog.config.MybatisDefaultDatasourceTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @MybatisDefaultDatasourceTest
 class UserRepositoryTest {
@@ -16,5 +17,24 @@ class UserRepositoryTest {
     @Test
     void successAutoWired() {
         assertThat(cut).isNotNull();
+    }
+
+    @Test
+    @DisplayName("selectByUsername: 指定されたユーザー名のユーザーが存在するとき、Optional<UserEntity> を返す")
+    @Sql(statements = {
+            "INSERT INTO users (id, username, password, enabled) VALUES(999, 'test_user_1', 'test_user_1_pass', true);",
+            "INSERT INTO users (id, username, password, enabled) VALUES(998, 'test_user_2', 'test_user_2_pass', true);"
+    })
+    void selectByUsername_success() {
+        // ## Arrange ##
+        // ## Act ##
+        var actual = cut.selectByUsername("test_user_1");
+
+        // ## Assert ##
+        assertThat(actual).hasValueSatisfying(actualEntity -> {
+            assertThat(actualEntity.username()).isEqualTo("test_user_1");
+            assertThat(actualEntity.password()).isEqualTo("test_user_1_pass");
+            assertThat(actualEntity.enabled()).isTrue();
+        });
     }
 }
