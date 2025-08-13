fea237889f7e5db27cbfa91040989809d1cf7510
tokotoko666 <cappuccino969@gmail.com>
2025-08-13 09:21:02 +0900

    UserServiceのテストを書く_テストに必要な設定だけをロードするように工夫する

---
+++ src/test/java/com/example/blog/service/UserServiceTest.java
@@ -0,0 +1,56 @@
+package com.example.blog.service;
+
+import com.example.blog.config.MybatisDefaultDatasourceTest;
+import com.example.blog.config.PasswordEncoderConfig;
+import com.example.blog.repository.user.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@MybatisDefaultDatasourceTest
+@Import({UserService.class, PasswordEncoderConfig.class})
+class UserServiceTest {
+
+    @Autowired
+    private UserService cut;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ApplicationContext ctx;
+
+
+    @Test
+    void successAutowired() {
+        assertThat(cut).isNotNull();
+        System.out.println("bean length " + ctx.getBeanDefinitionNames().length);
+    }
+
+    @Test
+    @DisplayName("register: ユーザーがデータベースに登録される")
+    void register_success() {
+        // ## Arrange ##
+        var username = "test_username";
+        var password = "test_password";
+
+        // ## Act ##
+        cut.register(username, password);
+
+        // ## Assert ##
+        var actual = userRepository.selectByUsername(username);
+        assertThat(actual).hasValueSatisfying(actualEntity -> {
+            assertThat(actualEntity.getPassword())
+                    .describedAs("入力された生のパスワードがハッシュ化されていること")
+                    .isNotEmpty()
+                    .isNotEqualTo(password);
+            assertThat(actualEntity.isEnabled())
+                    .describedAs("ユーザー登録時には、有効なアカウントとして登録する")
+                    .isTrue();
+        });
+    }
+}
