b671b91034a24cd820e69f9cf71c266b36b46fe3
tokotoko666 <cappuccino969@gmail.com>
2025-08-17 10:54:36 +0900

    バリデーション_usernameの先頭末尾に使えない文字

--- src/test/java/com/example/blog/web/user/UserFormTest.java
+++ src/test/java/com/example/blog/web/user/UserFormTest.java
@@ -53,6 +53,12 @@ class UserFormTest {
             "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
             "username!",
             "Username",
+            ".username",
+            "-username",
+            "_username",
+            "username.",
+            "username-",
+            "username_",
     })
     void username_failure(String username) {
         // ## Arrange ##
