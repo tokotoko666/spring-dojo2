62385c4e516c2d40de44357a4b2e667fe25b953b
tokotoko666 <cappuccino969@gmail.com>
2025-08-26 10:43:36 +0900

    UserRepository の修正_テストクラスを用意する

---
+++ src/test/java/com/example/blog/repository/user/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.example.blog.repository.user;
+
+import com.example.blog.config.MybatisDefaultDatasourceTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@MybatisDefaultDatasourceTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository cut;
+
+    @Test
+    void successAutowired() {
+        assertThat(cut).isNotNull();
+    }
+}
