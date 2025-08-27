ea3967b1788c29cfeedff62c956890807f3a30e6
tokotoko666 <cappuccino969@gmail.com>
2025-08-27 22:15:48 +0900

    ユーザー登録_UserController のテスト_URIの組み立てにUriComponentsBuilderを使う

--- src/main/java/com/example/blog/web/controller/user/UserRestController.java
+++ src/main/java/com/example/blog/web/controller/user/UserRestController.java
@@ -4,8 +4,8 @@ import com.example.blog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.security.Principal;
 
 @RestController
@@ -23,7 +23,12 @@ public class UserRestController {
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody UserForm userForm) {
         var newUser = userService.register(userForm.username(), userForm.password());
-        return ResponseEntity.created(URI.create("/users/" + newUser.getId())).build();
+        var location = UriComponentsBuilder.fromPath("/users/{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity
+                .created(location)
+                .build();
     }
 
 }
