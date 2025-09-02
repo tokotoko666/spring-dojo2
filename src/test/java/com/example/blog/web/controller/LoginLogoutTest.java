46a389f1d93e93430e96993dc9a4831f8d371886
tokotoko666 <cappuccino969@gmail.com>
2025-09-02 11:38:13 +0900

    ログイン_SecurityMockMvcResultMatchersを使ってログイン状態になっているかをアサートする

--- src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
+++ src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
@@ -1,28 +1,42 @@
 package com.example.blog.web.controller;
 
 import com.example.blog.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@Transactional
 class LoginLogoutTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void beforeEach() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
     void mockMvc() {
@@ -53,6 +67,7 @@ class LoginLogoutTest {
 
         // ## Assert ##
         actual
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(authenticated().withUsername(username));
     }
 }
