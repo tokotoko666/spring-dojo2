821c07f7706bd68b1aef9dee765dc34390a8414e
tokotoko666 <cappuccino969@gmail.com>
2025-09-21 11:55:02 +0900

    POST:articles:バリデーションのテストパターンを増やす

---
+++ src/test/java/com/example/blog/web/controller/article/ArticleFormTest.java
@@ -0,0 +1,73 @@
+package com.example.blog.web.controller.article;
+
+import com.example.blog.model.ArticleForm;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArticleFormTest {
+
+    private ValidatorFactory validatorFactory;
+    private Validator validator;
+
+    @BeforeEach
+    void beforeEach() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @AfterEach
+    void afterEach() {
+        validatorFactory.close();
+    }
+
+    @ParameterizedTest
+    @DisplayName("username のバリデーション：成功")
+    @ValueSource(strings = {
+            // 1文字
+            "あ",
+            // 255文字
+            "あいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえお",
+    })
+    void title_success(String title) {
+        // ## Arrange ##
+        var cut = new ArticleForm(title, "");
+
+        // ## Act ##
+        var actual = validator.validate(cut);
+
+        // ## Assert ##
+        assertThat(actual).isEmpty();
+    }
+
+    @ParameterizedTest
+    @DisplayName("title のバリデーション：失敗")
+    @NullSource
+    @ValueSource(strings = {
+            // 1文字
+            "",
+            // 256文字
+            "あいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおかきくけこさしすせそたちつてとなにぬねのあいうえおか"
+    })
+    void title_failure(String title) {
+        // ## Arrange ##
+        var cut = new ArticleForm(title, "");
+
+        // ## Act ##
+        var actual = validator.validate(cut);
+
+        // ## Assert ##
+        assertThat(actual).isNotEmpty();
+        assertThat(actual)
+                .anyMatch(violation -> violation.getPropertyPath().toString().equals("title"));
+    }
+
+}
