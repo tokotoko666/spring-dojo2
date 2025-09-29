48f6ab3dfaaac783e1ef6c10997a71c51e51fe58
tokotoko666 <cappuccino969@gmail.com>
2025-09-29 21:49:57 +0900

    POST :articles: ArticleEntityの修正_ArticleEntityをrecordではなくclassにする

--- src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceNoMockTest.java
+++ src/test/java/com/example/blog/web/controller/article/service/article/ArticleServiceNoMockTest.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -39,11 +38,11 @@ class ArticleServiceNoMockTest {
         // ## Assert ##
         assertThat(actual).isPresent()
                 .hasValueSatisfying(article -> {
-                    assertThat(article.id()).isEqualTo(999);
-                    assertThat(article.title()).isEqualTo("title_999");
-                    assertThat(article.content()).isEqualTo("body_999");
-                    assertThat(article.createdAt()).isEqualTo("2022-01-01T10:00:00");
-                    assertThat(article.updatedAt()).isEqualTo("2022-02-01T11:00:00");
+                    assertThat(article.getId()).isEqualTo(999);
+                    assertThat(article.getTitle()).isEqualTo("title_999");
+                    assertThat(article.getContent()).isEqualTo("body_999");
+                    assertThat(article.getCreatedAt()).isEqualTo("2022-01-01T10:00:00");
+                    assertThat(article.getUpdatedAt()).isEqualTo("2022-02-01T11:00:00");
                 });
     }
 
