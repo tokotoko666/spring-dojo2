7927ede8bee049fb9950a5ba5b949703df8f32fa
tokotoko666 <cappuccino969@gmail.com>
2025-11-05 09:22:02 +0900

    POST: comment_201 Created: Service 層のテストを書く

---
+++ src/test/java/com/example/blog/service/article/ArticleCommentServiceTest.java
@@ -0,0 +1,63 @@
+package com.example.blog.service.article;
+
+import com.example.blog.config.MybatisDefaultDatasourceTest;
+import com.example.blog.config.PasswordEncoderConfig;
+import com.example.blog.repository.article.ArticleRepository;
+import com.example.blog.service.DateTimeService;
+import com.example.blog.service.user.UserService;
+import com.example.blog.util.TestDateTimeUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@MybatisDefaultDatasourceTest
+@Import({ArticleCommentService.class, ArticleService.class, UserService.class, PasswordEncoderConfig.class})
+class ArticleCommentServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ArticleService articleService;
+
+    @MockBean
+    private DateTimeService mockDateTimeService;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private ArticleCommentService cut;
+
+    @Test
+    @DisplayName("create: 記事の登録に成功するとデータベースの articles テーブルにレコードが insert される")
+    void create_success() {
+        // ## Arrange ##
+        var expectedCurrentDateTime = TestDateTimeUtil.of(2020, 1, 2, 10, 20, 30);
+        when(mockDateTimeService.now()).thenReturn(expectedCurrentDateTime);
+
+        var articleAuthor = userService.register("test_username1", "test_password");
+        var commentAuthor = userService.register("test_username2", "test_password");
+        var article = articleService.create(articleAuthor.getId(), "test_title", "test_body");
+        var expectedComment = "コメントしました";
+
+        // ## Act ##
+        var actual = cut.create(commentAuthor.getId(), article.getId(), expectedComment);
+
+        // ## Assert ##
+
+        // assertion for return value
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getBody()).isEqualTo(expectedComment);
+        assertThat(actual.getAuthor())
+                .usingRecursiveComparison()
+                .ignoringFields("password")
+                .isEqualTo(commentAuthor);
+        assertThat(actual.getCreatedAt()).isEqualTo(expectedCurrentDateTime);
+    }
+}
