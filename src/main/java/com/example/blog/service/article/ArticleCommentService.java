0032432e24f0ca7da3575652405fd83f2491db7d
tokotoko666 <cappuccino969@gmail.com>
2025-11-05 09:34:23 +0900

    POST: comment_201 Created: Service 層を実装する

--- src/main/java/com/example/blog/service/article/ArticleCommentService.java
+++ src/main/java/com/example/blog/service/article/ArticleCommentService.java
@@ -1,21 +1,30 @@
 package com.example.blog.service.article;
 
+import com.example.blog.repository.article.ArticleCommentRepository;
+import com.example.blog.service.DateTimeService;
 import com.example.blog.service.user.UserEntity;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.OffsetDateTime;
-
 @Service
+@RequiredArgsConstructor
 public class ArticleCommentService {
 
+    private final ArticleCommentRepository articleCommentRepository;
+    private final DateTimeService dateTimeService;
+
     public ArticleCommentEntity create(long userId, long articleId, @NotNull String body) {
-        return new ArticleCommentEntity(
+        var newComment = new ArticleCommentEntity(
                 null,
                 body,
                 new ArticleEntity(articleId, "", "", null, null, null),
                 new UserEntity(userId, "", "", true),
-                OffsetDateTime.now()
+                dateTimeService.now()
         );
+
+        articleCommentRepository.insert(newComment);
+
+        return newComment;
     }
 }
