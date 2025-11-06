397d94c8bc39498e4451519ef54395f1e800c85c
tokotoko666 <cappuccino969@gmail.com>
2025-11-07 08:48:49 +0900

    POST: comment_400 BadRequest: テストを書く

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerCreateArticleCommentTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerCreateArticleCommentTest.java
@@ -80,4 +80,44 @@ class ArticleRestControllerCreateArticleCommentTest {
         ;
     }
 
+    @Test
+    @DisplayName("POST /articles/{articleId}/comments: リクエストの body フィールドが空のとき、400 BadRequest")
+    void createArticleComments_400BadRequest() throws Exception {
+        // ## Arrange ##
+        var articleAuthor = userService.register("test_username", "test_password");
+        var article = articleService.create(articleAuthor.getId(), "test_article_title", "test_article_body");
+        var commentAuthor = userService.register("test_username2", "test_password2");
+        var loggedInCommentAuthor = new LoggedInUser(commentAuthor.getId(), commentAuthor.getUsername(), commentAuthor.getPassword(), commentAuthor.isEnabled());
+
+        var bodyJson = """
+                {
+                  "body": ""
+                }
+                """;
+
+        // ## Act ##
+        var actual = mockMvc.perform(
+                post("/articles/{articleId}/comments", article.getId())
+                        .with(csrf())
+                        .with(user(loggedInCommentAuthor))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(bodyJson));
+
+        // ## Assert ##
+        actual
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(jsonPath("$.title").value("Bad Request"))
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.detail").value("Invalid request content."))
+                .andExpect(jsonPath("$.instance").value("/article/%d/comments".formatted(article.getId())))
+                .andExpect(jsonPath("$.errors", hasItem(
+                        allOf(
+                                hasEntry("pointer", "#/body"),
+                                hasEntry("detail", "コメント本文は必須です")
+                        )
+                )))
+        ;
+
+    }
 }
