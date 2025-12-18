2f77aef089474dae46c76d4ee92e61706a587ef3
tokotoko666 <cappuccino969@gmail.com>
2025-12-18 10:31:08 +0900

    Presigned URL 取得時に指定したメタデータと異なるファイルを S3 にアップロードできないこと_ContentLength_IT実装

--- src/test/java/com/example/blog/it/UploadUserProfileImageIT.java
+++ src/test/java/com/example/blog/it/UploadUserProfileImageIT.java
@@ -48,6 +48,7 @@ public class UploadUserProfileImageIT {
     private static final String DUMMY_SESSION_ID = "session_id_1";
     private static final String SESSION_COOKIE_NAME = "SESSION";
     private static final String TEST_IMAGE_FILE_NAME = "test.png";
+    private static final String TEST_IMAGE_FILE_NAME_OTHER_SIZE = "test_otherSize.png";
 
     @BeforeEach
     public void beforeEach() {
@@ -79,7 +80,7 @@ public class UploadUserProfileImageIT {
         var sessionId = loginSuccess(xsrfToken);
 
         // Pre-signed URL の取得
-        var uploadUrlDTO = getUserProfileImageUploadURL(sessionId, MediaType.IMAGE_PNG);
+        var uploadUrlDTO = getUserProfileImageUploadURL(sessionId, MediaType.IMAGE_PNG, TEST_IMAGE_FILE_NAME);
 
         // S3へのファイルアップロード
         uploadImage(uploadUrlDTO.getImageUploadUrl(), MediaType.IMAGE_PNG);
@@ -99,10 +100,29 @@ public class UploadUserProfileImageIT {
         var sessionId = loginSuccess(xsrfToken);
 
         // Pre-signed URL の取得
-        var uploadUrlDTO = getUserProfileImageUploadURL(sessionId, MediaType.IMAGE_PNG);
+        var uploadUrlDTO = getUserProfileImageUploadURL(sessionId, MediaType.IMAGE_PNG, TEST_IMAGE_FILE_NAME);
 
         // S3へのファイルアップロード
-        uploadImageContentTypeMismatch(uploadUrlDTO.getImageUploadUrl(), MediaType.APPLICATION_XML);
+        uploadImageContentTypeMismatch(uploadUrlDTO.getImageUploadUrl(), MediaType.APPLICATION_XML, TEST_IMAGE_FILE_NAME);
+
+    }
+
+    @Test
+    @DisplayName("Presigned URL 取得時に指定した ContentLength と異なるファイルは S3 にアップロードできない")
+    public void contentLengthMismatch() throws IOException {
+
+        var xsrfToken = getCsrfCookie();
+        // ユーザー作成
+        register(xsrfToken);
+
+        //ログイン成功
+        var sessionId = loginSuccess(xsrfToken);
+
+        // Pre-signed URL の取得
+        var uploadUrlDTO = getUserProfileImageUploadURL(sessionId, MediaType.IMAGE_PNG, TEST_IMAGE_FILE_NAME);
+
+        // S3へのファイルアップロード
+        uploadImageContentLengthMismatch(uploadUrlDTO.getImageUploadUrl(), MediaType.IMAGE_PNG, TEST_IMAGE_FILE_NAME_OTHER_SIZE);
 
     }
 
@@ -178,15 +198,20 @@ public class UploadUserProfileImageIT {
         return sessionIdOpt.get().getValue();
     }
 
-    private UserProfileUploadURLDTO getUserProfileImageUploadURL(String loginSessionCookie, MediaType contentType) {
+    private UserProfileUploadURLDTO getUserProfileImageUploadURL(String loginSessionCookie, MediaType contentType, String imageFileName) throws IOException {
+
+        // ## Arrange ##
+        var imageResource = new ClassPathResource(imageFileName);
+        var imageFile = imageResource.getFile();
+        var imageSize = imageFile.length();
 
         // ## Act ##
         var responseSpec = webTestClient
                 .get().uri(uriBuilder -> uriBuilder
                         .path("/users/me/image-upload-url")
-                        .queryParam("fileName", TEST_IMAGE_FILE_NAME)
+                        .queryParam("fileName", imageFileName)
                         .queryParam("contentType", contentType)
-                        .queryParam("contentLength", 104892)
+                        .queryParam("contentLength", imageSize)
                         .build()
                 )
                 .cookie(SESSION_COOKIE_NAME, loginSessionCookie)
@@ -204,7 +229,7 @@ public class UploadUserProfileImageIT {
                 .hasScheme("http")
                 .hasHost("localhost")
                 .hasPort(4566)
-                .hasPath("/profile-images/" + TEST_IMAGE_FILE_NAME)
+                .hasPath("/profile-images/" + imageFileName)
                 .hasParameter("X-Amz-Expires", "600")
                 .hasParameter("X-Amz-Signature");
 
@@ -236,9 +261,9 @@ public class UploadUserProfileImageIT {
         assertThat(actualImages).isEqualTo(imageBytes);
     }
 
-    private void uploadImageContentTypeMismatch(URI imageUploadUrl, MediaType contentType) throws IOException {
+    private void uploadImageContentTypeMismatch(URI imageUploadUrl, MediaType contentType, String imageFileName) throws IOException {
         // ## Arrange ##
-        var imageResource = new ClassPathResource(TEST_IMAGE_FILE_NAME);
+        var imageResource = new ClassPathResource(imageFileName);
         var imageFile = imageResource.getFile();
         var imageBytes = Files.readAllBytes(imageFile.toPath());
 
@@ -254,7 +279,31 @@ public class UploadUserProfileImageIT {
         // S3 にファイルがアップロードされていないことを確認する
         var request = HeadObjectRequest.builder()
                 .bucket(s3Properties.bucket().profileImages())
-                .key(TEST_IMAGE_FILE_NAME)
+                .key(imageFileName)
+                .build();
+        assertThatThrownBy(() -> testS3Client.headObject(request))
+                .isInstanceOf(NoSuchKeyException.class);
+    }
+
+    private void uploadImageContentLengthMismatch(URI imageUploadUrl, MediaType contentType, String imageFileName) throws IOException {
+        // ## Arrange ##
+        var imageResource = new ClassPathResource(imageFileName);
+        var imageFile = imageResource.getFile();
+        var imageBytes = Files.readAllBytes(imageFile.toPath());
+
+        // ## Act ##
+        var responseSpec = webTestClient.put().uri(imageUploadUrl)
+                .contentType(contentType)
+                .bodyValue(imageBytes)
+                .exchange();
+
+        // ## Assert ##
+        responseSpec.expectStatus().isForbidden();
+
+        // S3 にファイルがアップロードされていないことを確認する
+        var request = HeadObjectRequest.builder()
+                .bucket(s3Properties.bucket().profileImages())
+                .key(imageFileName)
                 .build();
         assertThatThrownBy(() -> testS3Client.headObject(request))
                 .isInstanceOf(NoSuchKeyException.class);
